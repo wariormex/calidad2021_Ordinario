940060b0837ea26610efb81a076af30edd15a7d7
wariormex <91165602+wariormex@users.noreply.github.com>
2021-11-25 19:17:25 -0600

    Update

--- src/main/java/com/anahuac/calidad/doubles/AlumnoDAO.java
+++
@@ -1,8 +0,0 @@
-package com.anahuac.calidad.doubles;
-
-public interface AlumnoDAO {
-	public boolean addAlumno(Alumno a);
-	public boolean deleteAlumno(Alumno a);
-	public boolean updateEmail(Alumno a);
-	public Alumno consultarAlumno(String id);
-}

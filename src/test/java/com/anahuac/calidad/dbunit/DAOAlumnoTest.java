d29cf688187568eab7aeab42af4d5ba90063b0d1
wariormex <91165602+wariormex@users.noreply.github.com>
2021-11-25 21:36:32 -0600

    Update localhost UNIX

--- src/test/java/com/anahuac/calidad/dbunit/DAOAlumnoTest.java
+++ src/test/java/com/anahuac/calidad/dbunit/DAOAlumnoTest.java
@@ -27,7 +27,7 @@ public class DAOAlumnoTest extends DBTestCase{
 	
 	public DAOAlumnoTest() {
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.cj.jdbc.Driver");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:33060/pruebas_db");
+		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, "jdbc:mysql://localhost:127.0.0.1/pruebas_db");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "secret");
 	}

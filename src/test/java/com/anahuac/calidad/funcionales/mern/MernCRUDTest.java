7771a0738f8eb70a6a5e20eb452c5db76c964631
wariormex <91165602+wariormex@users.noreply.github.com>
2021-11-27 00:10:51 -0600

    Restored

--- src/test/java/com/anahuac/calidad/funcionales/mern/MernCRUDTest.java
+++ src/test/java/com/anahuac/calidad/funcionales/mern/MernCRUDTest.java
@@ -161,12 +161,7 @@ public class MernCRUDTest {
 	  }
 	  
 	  @After
-	  public void tearDown() throws Exception {
-		driver.get(baseUrl);
-		pause(1000);
-		driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr/td[5]/button[2]")).click();
-		driver.findElement(By.xpath("/html/body/div[2]/div/div[3]/button[1]")).click();
-		pause(1000);	  
+	  public void tearDown() throws Exception {	  
 	    driver.quit();
 	    String verificationErrorString = verificationErrors.toString();
 	    if (!"".equals(verificationErrorString)) {
